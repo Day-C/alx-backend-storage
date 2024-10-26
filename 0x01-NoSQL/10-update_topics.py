cdf271c581f7a30973553bd39db39b795532a871
Dayc <kongranalanyuy@gmail.com>
2024-10-26 14:57:13 -0700

    update  document

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -6,4 +6,4 @@ import pymongo
 def update_topics(mongo_collection, name, topics):
     """Update all documents topic."""
 
-    updt =  mongo_collection.update_one({name:  name}, {"$set": {name: topics}})
+    updt =  mongo_collection.update_many({"name":  name}, {"$set": {"topics": topics}})
