f931cd6f602a3824d8f91568c5520bfc05c2742f
Dayc <kongranalanyuy@gmail.com>
2024-10-26 17:26:12 -0700

    cont documents

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,19 +3,25 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://localhost:27017/')
-# create db
-db = client.logs
-# create collection
-nginx = db.nginx
-methods = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
-# print the number of documents in collection
-print(f"{nginx.count_documents({})} logs")
-#print the count of each method
-print('Methods:')
-for key in methods.keys():
-    method_count = nginx.count_documents({"method": key})
-    print(f"    method {key}: {method_count}")
+def filter_mongo():
+    """filter and display information from mongodb."""
 
-lastLine = nginx.count_documents({"method": "GET", "path": "/status"})
-print(f"{lastLine} status check")
+    client = MongoClient('mongodb://localhost:27017/')
+    # create db
+    db = client.logs
+    # create collection
+    nginx = db.nginx
+    methods = {"GET": 0, "POST": 0, "PUT": 0, "PATCH": 0, "DELETE": 0}
+    # print the number of documents in collection
+    print(f"{nginx.count_documents({})} logs")
+    # print the count of each method
+    print('Methods:')
+    for key in methods.keys():
+        method_count = nginx.count_documents({"method": key})
+        print(f"    method {key}: {method_count}")
+
+    lastLine = nginx.count_documents({"method": "GET", "path": "/status"})
+    print(f"{lastLine} status check")
+
+
+filter_mongo()
